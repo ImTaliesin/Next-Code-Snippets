88a8794afe5b8c43ff24336ec69283e66ceb009a
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-01-30 14:44:45 -0800

    Ready for server actions

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,34 @@
 export default function SnippetCreatePage() {
-  return (
-    <div>Create a snippet</div>
-  );
+	return (
+		<form>
+			<h3 className='font-bold m-3'>Create a Snippet</h3>
+			<div className='flex flex-col gap-4'>
+				<div className='flex gap-4'>
+					<label
+						className='w-12'
+						htmlFor='title'>
+						Title:
+					</label>
+					<input
+						name='title'
+						className='border roundeed p-2 w-full'
+						id='title'
+					/>
+				</div>
+        <div className='flex gap-4'>
+					<label
+						className='w-12'
+						htmlFor='code'>
+						Code:
+					</label>
+					<textarea
+						name='title'
+						className='border roundeed p-2 w-full'
+						id='code'
+					/>
+				</div>
+        <button type="submit" className="rounded p2 bg-blue-600">Create</button>
+			</div>
+		</form>
+	);
 }
