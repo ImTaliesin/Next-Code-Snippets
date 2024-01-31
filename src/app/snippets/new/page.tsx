4ea81ce01c921eca728ba4efc2f108c660094640
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-01-31 13:43:15 -0800

    Uploading file to local db works

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,28 @@
+import { db } from '@/db';
+import { redirect } from 'next/navigation'
+
 export default function SnippetCreatePage() {
+
+	async function createSnippet(formData: FormData) {
+		//Server action
+		'use server';
+		//get the title and code from the form data
+		const title = formData.get('title') as string;
+		const code = formData.get('code') as string;
+		//create a new snippet in the database
+		const snippet = await db.snippet.create({
+			data: {
+				title,
+				code
+			},
+		});
+		console.log(snippet)
+		//redirect to root route
+		redirect('/')
+	}
+
 	return (
-		<form>
+		<form action={createSnippet}>
 			<h3 className='font-bold m-3'>Create a Snippet</h3>
 			<div className='flex flex-col gap-4'>
 				<div className='flex gap-4'>
@@ -15,19 +37,23 @@ export default function SnippetCreatePage() {
 						id='title'
 					/>
 				</div>
-        <div className='flex gap-4'>
+				<div className='flex gap-4'>
 					<label
 						className='w-12'
 						htmlFor='code'>
 						Code:
 					</label>
 					<textarea
-						name='title'
+						name='code'
 						className='border roundeed p-2 w-full'
 						id='code'
 					/>
 				</div>
-        <button type="submit" className="rounded p2 bg-blue-600">Create</button>
+				<button
+					type='submit'
+					className='rounded p2 bg-blue-600'>
+					Create
+				</button>
 			</div>
 		</form>
 	);
