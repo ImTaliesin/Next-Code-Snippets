f30ec38ffadf49df95c7c81d11445d997d836638
ImTaliesin <110573773+ImTaliesin@users.noreply.github.com>
2024-02-01 10:07:55 -0800

    Home page displays snippet, takes you to create a snippet. and lets you view snippits.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 import { db } from '@/db';
-import { Key } from 'react';
+import Link from 'next/link';
+
 export default async function Home() {
 	const snippets = await db.snippet.findMany();
-	const renderedSnippets = snippets.map(
-		(snippet: { id: Key; title: string; code: string }) => {
-			return (
-				<div key={snippet.id}>
-					<h3>{snippet.title}</h3>
-					<pre>{snippet.code}</pre>
-				</div>
-			);
-		}
-	);
 
-	return <div>{renderedSnippets}</div>;
+	const renderedSnippets = snippets.map((snippet: any) => {
+		return (
+			<Link
+				key={snippet.id}
+				href={`/snippets/${snippet.id}`}
+				className='flex justify-between items-center p-2 border rounded'>
+				<div>{snippet.title}</div>
+				<div>view</div>
+			</Link>
+		);
+	});
+
+	return (
+		<div>
+			<div className='flex justify-between items-center'>
+				<h1 className='text-xl font-bold'>Snippets</h1>
+				<Link
+					className='p-2 rounded'
+					href='/snippets/new'>
+					Create a Snippet
+				</Link>
+			</div>
+			<div className='flex flex-col gap-2'>{renderedSnippets}</div>
+		</div>
+	);
 }
--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,16 +1,21 @@
 import { db } from '@/db';
-
+import { notFound } from 'next/navigation';
 interface SnippetShowPageProps {
 	params: {
 		id: string;
 	};
 }
-export default async function SnipperShowPage(props: any) {
+export default async function SnippetShowPage(props: SnippetShowPageProps) {
+	await new Promise<void>((r) => setTimeout(r, 100));
 	const snippet = await db.snippet.findFirst({
 		where: {
 			id: parseInt(props.params.id)
 		},
 	});
+
+	if (!snippet) {
+		notFound();
+	}
 
 	return (<div>{snippet.title}<pre>{snippet.code}</pre></div>);
 }
